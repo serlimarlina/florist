bb981f429ae25caad7f6c1ab2066fe8375bdffdc
serli0607 <sirlymp16@gmail.com>
2024-05-28 09:32:04 +0700

    first commit

--- src/views/FloristView.vue
+++ src/views/FloristView.vue
@@ -1,118 +1,118 @@
 <template>
   <div class="relative">
-    <div class="w-full py-2 bg-indigo-400 text-center text-black text-xs">
-      Get 50% Discount now!
-    </div>
+    <div class="w-full py-2 bg-indigo-400 text-center text-black text-xs">Get 50% Discount now!</div>
     <div class="bg-white w-full sticky top-0 z-20 shadow">
       <div class="w-full lg:px-20 px-8 py-6 flex justify-between items-center">
-        <a aria-current="page" href="/" class="router-link-active router-link-exact-active cursor-pointer">
-          <img src="../assets/logo.png" alt="/" class="w-32 h-auto"></a>
-        <div class="gap-11 items-center justify-center text-sm lg:flex hidden">
-          <a v-for="container in containers" href="/" class="font-sans text-black-300 hover:opacity-10">{{ container.name }}</a>
-        </div> 
-        <div class="flex gap-10">
-          <a href="/" class="cursor-pointer hover:opacity-50"><i class="bi bi-search text-dark text-xl"></i></a>
-          <a href="/" class="cursor-pointer hover:opacity-50 relative"><i class="bi bi-cart text-dark text-xl"></i></a>
-        </div>
+        <img src="../assets/logo.png" alt="Aurora" class="w-32 h-auto">
+      <div class="gap-11 items-center justify-center text-sm lg:flex hidden">
+        <a v-for="container in containers" href="/" class="font-sans text-black-300 hover:opacity-10">{{ container.name }}</a>
+      </div> 
+      <div class="flex gap-10">
+        <a href="/" class="cursor-pointer hover:opacity-50"><i class="bi bi-search text-dark text-xl"></i></a>
+        <a href="/" class="cursor-pointer hover:opacity-50"><i class="bi bi-cart text-dark text-xl"></i></a>
       </div>
+    </div>
     </div>
     <section class="relative">
-    <img src="../assets/bg.jpg" alt="/" class="w-full lg:h-auto h-[265px] object-fill">
-    <div class="absolute z-10 w-full h-full top-1/3 lg:px-20 px-4">
-      <div class="bg-gradient-to-r from-indigo-300 p-2 w-60 text-center rounded"><i>where every bloom tells a story</i></div>
-      <div class="capitalize text-dark text-xl lg:text-4xl tracking-wide leading-snug">
-        <span class="font-sans">Blossom into </span><i class="font-sans bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-violet-700">Beauty</i> <br> 
-        <span class="font-sans">with Aurora</span></div>
-      <div class=" border-double border-4 border-indigo-600 cursor-pointer lg:p-2 p-2 w-80 lg:w-52 text-center text-black rounded lg:text-base text-xs mt-4 lg:mt-8 hover:opacity-70">
-        Make your flowers now! 
+      <img src="../assets/bg.jpg" alt="Background" class="w-full lg:h-auto h-[265px] object-cover">
+      <div class="absolute z-10 w-full h-full top-1/3 lg:px-20 px-4">
+        <div class="bg-gradient-to-r from-indigo-300 p-2 w-60 text-center rounded"><i>where every bloom tells a story</i></div>
+        <div class="capitalize text-dark text-xl lg:text-4xl tracking-wide leading-snug">
+          <span class="font-sans">Blossom into </span>
+          <i class="font-sans bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-violet-700">Beauty</i><br> 
+          <span class="font-sans">with Aurora</span></div>
+          <div class="border-double border-4 border-indigo-600 cursor-pointer lg:p-2 p-2 w-80 lg:w-52 text-center text-black rounded lg:text-base text-xs mt-4 lg:mt-8 hover:opacity-70">Make your flowers now! 
+        </div>
       </div>
-    </div>
-  </section>
-  <section class="bg-[url('src/assets/bg15.jpg')]">
-    <div class="text-2xl text-center py-10">
-      <span class="box-decoration-clone bg-gradient-to-r from-indigo-600 to-violet-500 text-white px-2">the benefits our shop offers</span></div>
-    <div class="w-full grid grid-cols-2 lg:flex gap-6 lg:gap-1 justify-between px-20 pb-11">
-      <div class="bg-indigo-300 w-64 text-center text-white rounded text-lg px-5 py-5"><i class="bi bi-clipboard-heart text-4xl"></i><br>Online Ordering Conveniences</div>
-      <div class="bg-indigo-300 w-64 text-center text-white rounded text-lg px-5 py-5"><i class="bi bi-truck text-4xl"></i><br>Timely Delivery Services</div>
-      <div class="bg-indigo-300 w-64 text-center text-white rounded text-lg px-5 py-5"><i class="bi bi-flower1 text-4xl"></i><br>best quality</div>
-      <div class="bg-indigo-300 w-64 text-center text-white rounded text-lg px-5 py-5"><i class="bi bi-person-hearts text-4xl"></i><br>Personalized Arrangements</div>
-    </div>
-  </section>
-  <section class="mx-24 my-20">
-    <div class="text-center text-2xl mb-10">Product category</div>
-    <div class="w-[100px] md:w-full overflow-x-auto">
-      <div class="flex w-full text-center">
-        <a v-for="(container, index) in containers" :key="index"  href="/" :class="getBackgroundClass(index)" class="text-lg bg-indigo-300 lg:p-10 p-8 flex lg:w-full w-32 justify-center items-center hover:opacity-50">{{ container.name }}</a>
+    </section>
+    <section class="bg-[url('src/assets/bg15.jpg')] py-10">
+      <div class="text-2xl text-center py-10">
+        <span class="box-decoration-clone bg-gradient-to-r from-indigo-600 to-violet-500 text-white px-2 ">The benefits our shop offers</span>
       </div>
-    </div>
-  </section>
-  <section v-for="(container, index) in containers" :key="container.index" class="py-8 lg:py-16 lg:px-20 px-4 flex flex-col gap-10">
-    <div class="text-center w-full text-xl text-dark">{{ container.name }}</div>
+      <div class="mt-6 lg:mt-12 lg:w-full grid grid-cols-2 lg:flex gap-6 lg:gap-10 justify-between lg:px-20 px-4">
+        <div class="bg-indigo-300 lg:w-[287px] rounded px-4 text-center text-white text-lg py-5"><i class="bi bi-clipboard-heart text-4xl"></i><br>Online Ordering Conveniences</div>
+        <div class="bg-indigo-300 lg:w-[287px] rounded px-4 text-center text-white text-lg py-5"><i class="bi bi-truck text-4xl"></i><br>Timely Delivery Services</div>
+        <div class="bg-indigo-300 lg:w-[287px] rounded px-4 text-center text-white text-lg py-5"><i class="bi bi-flower1 text-4xl"></i><br>Best Quality</div>
+        <div class="bg-indigo-300 lg:w-[287px] rounded px-4 text-center text-white text-lg py-5"><i class="bi bi-person-hearts text-4xl"></i><br>Personalized Arrangements</div>
+      </div>
+    </section>
+    <section class="mx-4 lg:mx-24 my-20">
+      <div class="text-center text-2xl mb-10">Product Category</div>
+      <div class="w-full overflow-x-auto">
+        <div class="flex w-max lg:w-full text-center">
+          <a v-for="(container, index) in containers" :key="index" href="/" :class="getBackgroundClass(index)" class="text-lg bg-indigo-300 lg:p-10 p-8 flex lg:w-full w-32 justify-center items-center hover:opacity-50">{{ container.name }}</a>
+        </div>
+      </div>
+    </section>
+    <section v-for="(container, index) in containers" :key="index" class="py-8 lg:py-16 lg:px-20 px-4 flex flex-col gap-10">
+      <div class="text-center w-full text-xl text-dark">{{ container.name }}</div>
       <div class="grid grid-cols-2 lg:grid-cols-5 gap-4">
-      <div v-for="(product, index) in container.products" :key="product.index" :class="{'hidden lg:block': product.index === 4}">
-        <img :src="product.image" :alt="product.name" class="cursor-pointer hover:opacity-70 w-full h-56 object-cover image-lazy pulse" lazy="loaded">
+        <div v-for="(product, index) in container.products" :key="index" :class="{'hidden lg:block': index === 4}">
+          <img :src="product.image" :alt="product.name" class="cursor-pointer hover:opacity-70 w-full h-56 object-cover">
           <div class="bg-indigo-200 w-full p-4">
             <div class="mb-1">{{ product.name }}</div>
             <div class="flex items-center mb-1 space-x-1">
-              <i v-for="i in 5" :key="i" class="bi bi-star-fill text-yellow-400"></i></div>
-              <div class="flex items-center justify-between">{{ product.price }}
+              <i v-for="i in 5" :key="i" class="bi bi-star-fill text-yellow-400"></i>
+            </div>
+            <div class="flex items-center justify-between">{{ product.price }}
               <div class="bg-indigo-500 w-8 h-8 flex items-center justify-center rounded-full hover:opacity-50">
-              <i class="bi bi-cart-plus text-lg text-white"></i>
+                <i class="bi bi-cart-plus text-lg text-white"></i>
+              </div>
             </div>
           </div>
         </div>
+      </div><div class="text-center hover:opacity-50"><a href="/">See All Product</a></div>
+    </section>
+    <section class="mx-4 lg:mx-24 my-20">
+      <div class="text-xl font-bold mb-6">Aurora Florist - Online Flower Shop Based in Indonesia</div>
+      <div class="text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, quisquam magnam fugiat quidem quasi excepturi quam alias quos nihil soluta, eos ab consequatur atque quia reiciendis ut adipisci, suscipit itaque?</div>
+    </section>
+    <section class="bg-[url('src/assets/bg-1.jpg')] py-10">
+      <div class="text-2xl text-center py-10">
+        <span class="box-decoration-clone bg-gradient-to-r from-indigo-600 to-violet-500 text-white px-2">Customer Testimonials</span>
       </div>
-    </div><div class="text-center hover:opacity-50"><a href="/">See All Product</a></div>
-  </section>
-  <section class="mx-24 my-20">
-    <div class="text-xl font-bold mb-6">Aurora Florist - online flower shop based in Indonesia</div>
-    <div class="text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, quisquam magnam fugiat quidem quasi excepturi quam alias quos nihil soluta, eos ab consequatur atque quia reiciendis ut adipisci, suscipit itaque?</div>
-  </section>
-  <section class="bg-[url('src/assets/bg-1.jpg')]">
-    <div class="text-2xl text-center py-10">
-      <span class="box-decoration-clone bg-gradient-to-r from-indigo-600 to-violet-500 text-white px-2">customer testimonials</span></div>
-      <div class="grid grid-rows-1 grid-flow-col gap-4 justify-center px-10 pb-11">
-        <div v-for="testimonial in testimonials" :key="testimonial.id" class="bg-indigo-400 w-full text-center text-white rounded text-lg px-5 py-5">
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 justify-center px-4 lg:px-10 pb-11">
+        <div v-for="testimonial in testimonials" :key="testimonial.id" class="bg-indigo-400 text-center text-white rounded text-lg px-5 py-5">
           <div class="w-full flex justify-center"><i class="bi bi-person-square text-4xl"></i></div>
           <div class="font-sans">{{ testimonial.name }}</div>
-        <div>{{ testimonial.message }}</div>
-      </div>
-    </div>
-  </section>
-  <section>
-    <div class="text-2xl text-center py-10">
-      <span class="box-decoration-clone bg-gradient-to-r from-indigo-600 to-violet-500 text-white px-2">Payment Gateway</span></div>
-        <img src="../assets/pay.png" alt="payment gateway" class="w-full h-56 "><br><br><br>
-  </section>
-  <section class="bg-indigo-100 px-20 py-10">
-    <div class="text-xl mb-10 text-center underline decoration-indigo-800">General Questions (FAQ)</div>
-    <div class="flex flex-col gap-4">
-      <div v-for="(faq, index) in faqs" :key="faq.id" class="bg-white cursor-pointer p-4 flex flex-col gap-2">
-        <div class="flex w-full justify-between items-center">
-          <div class="font-semibold">{{ faq.question }}</div>
-          <div><i class="bi bi-chevron-down"></i></div>
+          <div>{{ testimonial.message }}</div>
         </div>
       </div>
-    </div>
-  </section>
-  <section class="mx-20 my-20">
-    <div class="flex justify-between">
-      <div class="text-2xl"><b>🌟 Aurora: Let Your Moments Blossom 🌟</b></div>
-      <div class="bg-indigo-400 text-white px-5 lg:px-16 py-2 lg:py-3 rounded cursor-pointer hover:opacity-50">Buy Now!</div>
-    </div>
-  </section>
-  <footer>
-    <div class="bg-indigo-300 py-10 px-20">
-      <div class="flex items-center text-2xl font-bold uppercase text-black" href="/">
-        <img src="../assets/logo.png" alt="logo" class="w-52 h-auto"></div>
-      <div class="grid grid-rows-1 grid-flow-col gap-4 justify-between text-black my-10">
-        <div><p class="text-xl text-bold mb-5">Address</p><p class="text-lg">Cianjur, Jawa Barat</p></div>
-        <div><p class="text-xl text-semibold mb-5">Product category</p><p v-for="container in containers"  class="text-lg">{{ container.name }}</p></div>
-        <div><p class="text-xl text-semibold mb-5">customer Care</p><p class="text-lg">+62 123 456 78</p><p class="text-lg">minceu@florist.com</p></div>
+    </section>
+    <section class="px-4 lg:px-20 py-10">
+      <div class="text-2xl text-center py-10">
+        <span class="box-decoration-clone bg-gradient-to-r from-indigo-600 to-violet-500 text-white px-2">Payment Gateway</span>
       </div>
-      <div class="w-full text-center text-black text-xs"> © 2024 Aurora Florist All rights reserved </div>
-    </div>
-  </footer>
+      <img src="../assets/pay.png" alt="payment gateway" class="w-full h-56 object-cover mb-10">
+    </section>
+    <section class="bg-indigo-100 px-4 lg:px-20 py-10">
+      <div class="text-xl mb-10 text-center underline decoration-indigo-800">General Questions (FAQ)</div>
+      <div class="flex flex-col gap-4">
+        <div v-for="(faq, index) in faqs" :key="faq.id" class="bg-white cursor-pointer p-4 flex flex-col gap-2">
+          <div class="flex w-full justify-between items-center">
+            <div class="font-semibold">{{ faq.question }}</div>
+            <div><i class="bi bi-chevron-down"></i></div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="mx-4 lg:mx-20 my-20">
+      <div class="flex justify-between items-center">
+        <div class="text-2xl font-bold">🌟 Aurora: Let Your Moments Blossom 🌟</div>
+        <div class="bg-indigo-400 text-white px-5 lg:px-16 py-2 lg:py-3 rounded cursor-pointer hover:opacity-50">Buy Now!</div>
+      </div>
+    </section>
+    <footer>
+      <div class="bg-indigo-300 py-10 px-4 lg:px-20">
+        <div class="flex items-center text-2xl font-bold uppercase text-black"><img src="../assets/logo.png" alt="logo" class="w-32 lg:w-52 h-auto"></div>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 justify-between text-black my-10">
+          <div><p class="text-xl font-bold mb-5">Address</p><p class="text-lg">Cianjur, Jawa Barat</p></div>
+          <div><p class="text-xl font-semibold mb-5">Product Category</p><p v-for="container in containers" class="text-lg">{{ container.name }}</p></div>
+          <div><p class="text-xl font-semibold mb-5">Customer Care</p><p class="text-lg">+62 123 456 78</p><p class="text-lg">minceu@florist.com</p></div>
+        </div>
+        <div class="w-full text-center text-black text-xs">© 2024 Aurora Florist All rights reserved</div>
+      </div>
+    </footer>
   </div>
 </template>
 <script setup>
